ba62f1c3d5b797958201879a2a35b8e88ff8ec81
Dante Ibanez <dantedavidibanez2@gmail.com>
2023-05-11 11:04:00 -0700

    Introduce small class for explosion image

---
+++ Explosion.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.Image;
+
+/**Explosion.java
+ * A small java class to handle the explosion sprite
+ */
+public class Explosion{
+    //declare GLOBAL variables
+    public int x_coordinate = 0;
+    public int y_coordinate = 0;
+
+    public Image explosionInfo;
+
+    //constructor, load sprites
+    public Explosion(){
+        loadExplosion();
+    }
+
+    public void loadExplosion(){
+        ImageIcon explosionIcon = new ImageIcon("resources/explosion.gif");
+        explosionInfo = explosionIcon.getImage();
+    }
+
+    public int getX_coordinate(){
+        return x_coordinate;
+    }
+
+    public int getY_coordinate(){
+        return getY_coordinate();
+    }
+}
